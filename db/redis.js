aeaa26f9f79e7b796f3596f61b7769ffa60c053f
bholagabbar <dev.shreyans96@gmail.com>
2022-05-08 16:50:22 -0400

    Fix redis URL

--- db/redis.js
+++ db/redis.js
@@ -3,7 +3,9 @@ import redis from "redis"
 export class RedisConnection {
     // TODO: Cache
     static async getClient() {
-        const client = redis.createClient(process.env.REDIS_URI);
+        const client = redis.createClient({
+            url: process.env.REDIS_URI
+        });
         await client.connect()
         return client;
     }
